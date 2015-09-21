241faeb4e583165447f143a07c26c7a2ffd655c0
David Molineus <david.molineus@netzmacht.de>
2015-09-21 23:28:03 +0200

    Add hybrid.

---
+++ src/Netzmacht/Contao/Toolkit/Component/Hybrid.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+namespace Netzmacht\Contao\Toolkit\Component;
+
+/**
+ * Hybrid base class using the toolkit features.
+ *
+ * @package Netzmacht\Contao\Toolkit\Component
+ */
+abstract class Hybrid extends \Hybrid
+{
+    use ComponentTrait;
+}
