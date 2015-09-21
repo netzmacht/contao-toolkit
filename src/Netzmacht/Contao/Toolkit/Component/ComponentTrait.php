c7407eeb2806566a3ed3db10097f7244a577386e
David Molineus <david.molineus@netzmacht.de>
2015-09-21 23:08:55 +0200

    Implement default components.

---
+++ src/Netzmacht/Contao/Toolkit/Component/ComponentTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+namespace Netzmacht\Contao\Toolkit\Component;
+
+use Netzmacht\Contao\Toolkit\ServiceContainerTrait;
+use Netzmacht\Contao\Toolkit\View\Template;
+use Netzmacht\Contao\Toolkit\View\TemplateDecorator;
+
+/**
+ * ComponentTrait provides the toolkit way to compile components.
+ *
+ * @package Netzmacht\Contao\Toolkit\Component
+ */
+trait ComponentTrait
+{
+    use ServiceContainerTrait;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function compile()
+    {
+        $translator = $this->getServiceContainer()->getTranslator();
+        $template   = new TemplateDecorator($this->Template, $translator);
+
+        $this->render($template);
+    }
+
+    /**
+     * Compile the template.
+     *
+     * @param Template $template The template.
+     *
+     * @return void
+     */
+    abstract protected function render(Template $template);
+}
---
+++ src/Netzmacht/Contao/Toolkit/Component/ContentElement.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+namespace Netzmacht\Contao\Toolkit\Component;
+
+/**
+ * Class ContentElement using the toolkit template interface.
+ *
+ * @package Netzmacht\Contao\Toolkit\Component
+ */
+abstract class ContentElement extends \ContentElement
+{
+    use ComponentTrait;
+}
