f3e3e944800e806fec3815d5fc891397796ccbce
David Molineus <david.molineus@netzmacht.de>
2015-09-24 09:00:11 +0200

    Add base callbacks class.

---
+++ src/Netzmacht/Contao/Toolkit/Dca/Callbacks.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+namespace Netzmacht\Contao\Toolkit\Dca;
+
+use Netzmacht\Contao\Toolkit\ServiceContainerTrait;
+use Netzmacht\Contao\Toolkit\TranslatorTrait;
+
+/**
+ * Base class for data container callback classes.
+ *
+ * @package Netzmacht\Contao\Toolkit\Dca
+ */
+abstract class Callbacks
+{
+    use ServiceContainerTrait;
+    use TranslatorTrait;
+
+    /**
+     * Generate the callback definition.
+     *
+     * @param string $name Callback method.
+     *
+     * @return array
+     */
+    public static function callback($name)
+    {
+        return [get_called_class(), $name];
+    }
+}
