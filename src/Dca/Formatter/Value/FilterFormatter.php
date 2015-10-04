1960616cd0696d958fe7e0f22f3c5c58efb54287
David Molineus <david.molineus@netzmacht.de>
2015-10-04 15:47:27 +0200

    Simplify the filter formatter.

--- src/Dca/Formatter/Value/FilterFormatter.php
+++ src/Dca/Formatter/Value/FilterFormatter.php
@@ -12,45 +12,30 @@
 namespace Netzmacht\Contao\Toolkit\Dca\Formatter\Value;
 
 /**
- * FilterFormatter applies pre and post filters before and after formatting.
+ * FilterFormatter applies formatters used as filters to an value.
+ *
+ * The difference between the FormatterChain and the FilterFormatter is that the filter formatter will apply all
+ * rules and modifies the given value.
  *
  * @package Netzmacht\Contao\Toolkit\Dca\Formatter\Value
  */
 class FilterFormatter implements ValueFormatter
 {
     /**
-     * Pre filters are applied before formatting the value.
+     * List of filters.
      *
      * @var ValueFormatter[]
      */
-    private $preFilters = array();
-
-    /**
-     * Post filters are applied after formatting the value.
-     *
-     * @var ValueFormatter[]
-     */
-    private $postFilters = array();
-
-    /**
-     * Value formatter.
-     *
-     * @var ValueFormatter
-     */
-    private $formatter;
+    private $filters = array();
 
     /**
      * Construct.
      *
-     * @param ValueFormatter   $formatter   The formatter.
-     * @param ValueFormatter[] $preFilters  Formatters are applied as filter before formatting.
-     * @param ValueFormatter[] $postFilters Formatters are applied as filter after formatting.
+     * @param ValueFormatter[] $filters List of filters.
      */
-    public function __construct(ValueFormatter $formatter, $preFilters = [], array $postFilters = [])
+    public function __construct(array $filters = [])
     {
-        $this->formatter   = $formatter;
-        $this->preFilters  = $preFilters;
-        $this->postFilters = $postFilters;
+        $this->filters = $filters;
     }
 
     /**
@@ -66,31 +51,7 @@ class FilterFormatter implements ValueFormatter
      */
     public function format($value, $fieldName, array $fieldDefinition, $context = null)
     {
-        $value = $this->applyFilters($this->preFilters, $value, $fieldName, $fieldDefinition, $context);
-
-        if ($this->formatter->accepts($fieldName, $fieldDefinition)) {
-            $value = $this->formatter->format($value, $fieldName, $fieldDefinition, $context);
-        }
-
-        $value = $this->applyFilters($this->postFilters, $value, $fieldName, $fieldDefinition, $context);
-
-        return $value;
-    }
-
-    /**
-     * Apply a set of filters.
-     *
-     * @param ValueFormatter[] $filters         Filter formatters.
-     * @param mixed            $value           Given value.
-     * @param string           $fieldName       Field name.
-     * @param array            $fieldDefinition Field definition.
-     * @param mixed            $context         Context of the call. Usually the data container driver.
-     *
-     * @return mixed
-     */
-    private function applyFilters(array $filters, $value, $fieldName, $fieldDefinition, $context)
-    {
-        foreach ($filters as $filter) {
+        foreach ($this->filters as $filter) {
             if ($filter->accepts($fieldName, $fieldDefinition)) {
                 $value = $filter->format($value, $fieldName, $fieldDefinition, $context);
             }
