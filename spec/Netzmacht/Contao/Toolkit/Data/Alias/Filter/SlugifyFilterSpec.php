e45c9f2b97f4ceac903da0cfba02bfe9c867852d
David Molineus <david.molineus@netzmacht.de>
2015-10-17 13:29:25 +0200

    Replace Contao standardize method with own implementation as addingthe id is not wanted.

--- spec/Netzmacht/Contao/Toolkit/Data/Alias/Filter/SlugifyFilterSpec.php
+++ spec/Netzmacht/Contao/Toolkit/Data/Alias/Filter/SlugifyFilterSpec.php
@@ -7,28 +7,6 @@ use Netzmacht\Contao\Toolkit\Data\Alias\Filter\SlugifyFilter;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
-if (!function_exists('standardize')) {
-    function standardize($str)
-    {
-        $a = array('À','Á','Â','Ã','Ä','Å','Æ','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ð','Ñ','Ò','Ó','Ô','Õ','Ö','Ø','Ù','Ú','Û','Ü','Ý','ß','à','á','â','ã','ä','å','æ','ç','è','é','ê','ë','ì','í','î','ï','ñ','ò','ó','ô','õ','ö','ø','ù','ú','û','ü','ý','ÿ','A','a','A','a','A','a','C','c','C','c','C','c','C','c','D','d','Ð','d','E','e','E','e','E','e','E','e','E','e','G','g','G','g','G','g','G','g','H','h','H','h','I','i','I','i','I','i','I','i','I','i','?','?','J','j','K','k','L','l','L','l','L','l','?','?','L','l','N','n','N','n','N','n','?','O','o','O','o','O','o','Œ','œ','R','r','R','r','R','r','S','s','S','s','S','s','Š','š','T','t','T','t','T','t','U','u','U','u','U','u','U','u','U','u','U','u','W','w','Y','y','Ÿ','Z','z','Z','z','Ž','ž','?','ƒ','O','o','U','u','A','a','I','i','O','o','U','u','U','u','U','u','U','u','U','u','?','?','?','?','?','?');
-        $b = array('A','A','A','A','A','A','AE','C','E','E','E','E','I','I','I','I','D','N','O','O','O','O','O','O','U','U','U','U','Y','s','a','a','a','a','a','a','ae','c','e','e','e','e','i','i','i','i','n','o','o','o','o','o','o','u','u','u','u','y','y','A','a','A','a','A','a','C','c','C','c','C','c','C','c','D','d','D','d','E','e','E','e','E','e','E','e','E','e','G','g','G','g','G','g','G','g','H','h','H','h','I','i','I','i','I','i','I','i','I','i','IJ','ij','J','j','K','k','L','l','L','l','L','l','L','l','l','l','N','n','N','n','N','n','n','O','o','O','o','O','o','OE','oe','R','r','R','r','R','r','S','s','S','s','S','s','S','s','T','t','T','t','T','t','U','u','U','u','U','u','U','u','U','u','U','u','W','w','Y','y','Y','Z','z','Z','z','Z','z','s','f','O','o','U','u','A','a','I','i','O','o','U','u','U','u','U','u','U','u','U','u','A','a','AE','ae','O','o');
-
-        return strtolower(
-            preg_replace(
-                array('/[^a-zA-Z0-9 -]/','/[ -]+/','/^-|-$/'),array('','-',''),
-                str_replace($a,$b,$str)
-            )
-        );
-    }
-
-    eval(
-        'function standardize ($str) {
-            return spec\Netzmacht\Contao\Toolkit\Data\Alias\Filter\standardize($str);
-        }
-        '
-    );
-}
-
 /**
  * Class SlugifyFilterSpec
  * @package spec\Netzmacht\Contao\Toolkit\Data\Alias\Filter
@@ -40,9 +18,14 @@ class SlugifyFilterSpec extends ObjectBehavior
 
     const RAW_VALUE = 'Äöü -?^°#.test';
 
-    const ALIAS_VALUE = 'alias-value';
+    const ALIAS_VALUE = 'aeoeue-test';
 
     const COLUMN = 'title';
+
+    function let()
+    {
+        $GLOBALS['TL_CONFIG']['characterSet'] = 'utf-8';
+    }
 
     function createInstance()
     {
@@ -80,37 +63,39 @@ class SlugifyFilterSpec extends ObjectBehavior
         $this->repeatUntilValid()->shouldReturn(false);
     }
 
-    function it_standardizes_value()
+    function it_slugifies_value()
     {
         $model = (object) [static::COLUMN => static::RAW_VALUE];
 
         $this->createInstance();
-        $this->apply($model, '', static::SEPARATOR)->shouldReturn(standardize(static::RAW_VALUE));
+        $this->apply($model, '', static::SEPARATOR)->shouldReturn(static::ALIAS_VALUE);
     }
 
     function it_supports_multiple_columns()
     {
-        $model = (object) [
+        $aliasValue = 'aeae0032ae-12';
+        $model      = (object) [
             static::COLUMN => static::RAW_VALUE,
             'test' => 'ää0032ä-´12'
         ];
 
         $this->beConstructedWith([static::COLUMN, 'test']);
         $this->apply($model, '', static::SEPARATOR)->shouldReturn(
-            standardize(static::RAW_VALUE) . static::SEPARATOR . standardize($model->test)
+            static::ALIAS_VALUE . static::SEPARATOR . $aliasValue
         );
     }
 
     function it_supports_custom_separator()
     {
-        $model = (object) [
+        $aliasValue = 'aeae0032ae_12';
+        $model       = (object) [
             static::COLUMN => static::RAW_VALUE,
             'test' => 'ää0032ä-´12'
         ];
 
         $this->beConstructedWith([static::COLUMN, 'test']);
         $this->apply($model, '', '_')->shouldReturn(
-            standardize(static::RAW_VALUE) . '_' . standardize($model->test)
+            str_replace('-', '_', static::ALIAS_VALUE) . '_' . $aliasValue
         );
     }
 
@@ -123,7 +108,7 @@ class SlugifyFilterSpec extends ObjectBehavior
 
         $this->beConstructedWith([static::COLUMN, 'id']);
         $this->apply($model, 'test', '_')->shouldReturn(
-            standardize(static::RAW_VALUE) . '_' . 5
+            str_replace('-', '_', static::ALIAS_VALUE) . '_' . 5
         );
     }
 
--- src/Data/Alias/Filter/SlugifyFilter.php
+++ src/Data/Alias/Filter/SlugifyFilter.php
@@ -26,22 +26,32 @@ class SlugifyFilter extends AbstractValueFilter
     private $preserveUppercase;
 
     /**
+     * Encoding charset.
+     *
+     * @var string
+     */
+    private $charset;
+
+    /**
      * Construct.
      *
-     * @param array $columns           Columns being used for the value.
-     * @param bool  $break             If true break after the filter if value is unique.
-     * @param int   $combine           Combine flag.
-     * @param bool  $preserveUppercase If true uppercase values are not transformed.
+     * @param array  $columns           Columns being used for the value.
+     * @param bool   $break             If true break after the filter if value is unique.
+     * @param int    $combine           Combine flag.
+     * @param bool   $preserveUppercase If true uppercase values are not transformed.
+     * @param string $charset           Encoding charset.
      */
     public function __construct(
         array $columns,
         $break = true,
         $combine = self::COMBINE_REPLACE,
-        $preserveUppercase = false
+        $preserveUppercase = false,
+        $charset = 'utf-8'
     ) {
         parent::__construct($columns, $break, $combine);
 
         $this->preserveUppercase = (bool) $preserveUppercase;
+        $this->charset           = $charset;
     }
 
     /**
@@ -52,9 +62,34 @@ class SlugifyFilter extends AbstractValueFilter
         $values = array();
 
         foreach ($this->columns as $column) {
-            $values[] = standardize($model->$column, $this->preserveUppercase);
+            $values[] = $this->slugify($model->$column, $separator);
         }
 
         return $this->combine($value, $values, $separator);
+    }
+
+    /**
+     * Slugify a value.
+     *
+     * @param string $value     Given value.
+     * @param string $separator Separator string.
+     *
+     * @return string
+     */
+    private function slugify($value, $separator)
+    {
+        $arrSearch  = array('/[^a-zA-Z0-9 \.\&\/_-]+/', '/[ \.\&\/-]+/');
+        $arrReplace = array('', $separator);
+
+        $value = html_entity_decode($value, ENT_QUOTES, $this->charset);
+        $value = strip_insert_tags($value);
+        $value = utf8_romanize($value);
+        $value = preg_replace($arrSearch, $arrReplace, $value);
+
+        if (!$this->preserveUppercase) {
+            $value = strtolower($value);
+        }
+
+        return trim($value, $separator);
     }
 }
