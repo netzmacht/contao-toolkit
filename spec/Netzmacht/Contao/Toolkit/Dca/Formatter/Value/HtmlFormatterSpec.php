7ccd0af2f0d60fdb181228edc560f3f9a7dcbb2d
David Molineus <david.molineus@netzmacht.de>
2015-10-20 21:52:36 +0200

    Describe html formatter.

---
+++ spec/Netzmacht/Contao/Toolkit/Dca/Formatter/Value/HtmlFormatterSpec.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace spec\Netzmacht\Contao\Toolkit\Dca\Formatter\Value;
+
+use Netzmacht\Contao\Toolkit\Dca\Formatter\Value\HtmlFormatter;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/**
+ * Class HtmlFormatterSpec
+ *
+ * @package spec\Netzmacht\Contao\Toolkit\Dca\Formatter\Value
+ * @mixin HtmlFormatter
+ */
+class HtmlFormatterSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Netzmacht\Contao\Toolkit\Dca\Formatter\Value\HtmlFormatter');
+    }
+
+    function it_is_a_value_formatter()
+    {
+        $this->shouldImplement('Netzmacht\Contao\Toolkit\Dca\Formatter\Value\ValueFormatter');
+    }
+
+    function it_accepts_fields_with_allow_html_option()
+    {
+        $definition['eval']['allowHtml'] = true;
+
+        $this->accepts('test', $definition)->shouldReturn(true);
+    }
+
+    function it_accepts_fields_preserve_tags_option()
+    {
+        $definition['eval']['preserveTags'] = true;
+
+        $this->accepts('test', $definition)->shouldReturn(true);
+    }
+
+    function it_encodes_html_entities()
+    {
+        $test = '<b>Test</b> <a href="http://example.org">Example</a>';
+
+        $this->format($test, 'test', [])->shouldReturn(htmlentities($test));
+    }
+}
