64e905f80fda793ab8560f706638f85e4fbea587
David Molineus <david.molineus@netzmacht.de>
2015-10-21 09:24:59 +0200

    Describe filter formatter.

---
+++ spec/Netzmacht/Contao/Toolkit/Dca/Formatter/Value/FilterFormatterSpec.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace spec\Netzmacht\Contao\Toolkit\Dca\Formatter\Value;
+
+use Netzmacht\Contao\Toolkit\Dca\Formatter\Value\FilterFormatter;
+use Netzmacht\Contao\Toolkit\Dca\Formatter\Value\ValueFormatter;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/**
+ * Class FilterFormatterSpec
+ *
+ * @package spec\Netzmacht\Contao\Toolkit\Dca\Formatter\Value
+ * @mixin FilterFormatter
+ */
+class FilterFormatterSpec extends ObjectBehavior
+{
+    function let(ValueFormatter $formatterA, ValueFormatter $formatterB)
+    {
+        $this->beConstructedWith([$formatterA, $formatterB]);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Netzmacht\Contao\Toolkit\Dca\Formatter\Value\FilterFormatter');
+    }
+
+    function it_is_a_value_formatter()
+    {
+        $this->shouldImplement('Netzmacht\Contao\Toolkit\Dca\Formatter\Value\ValueFormatter');
+    }
+
+    function it_accepts_anything()
+    {
+        $this->accepts('test', [])->shouldReturn(true);
+    }
+
+    function it_applies_matching_formatters(ValueFormatter $formatterA, ValueFormatter $formatterB)
+    {
+        $formatterA->accepts(Argument::cetera())->willReturn(false);
+        $formatterA->format(Argument::cetera())->shouldNotBeCalled();
+
+        $formatterB->accepts(Argument::cetera())->willReturn(true);
+        $formatterB->format(Argument::cetera())->shouldBeCalled()->willReturn('bar');
+
+        $this->format('foo', 'test', [])->shouldReturn('bar');
+    }
+}
