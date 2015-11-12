703d4747b583f029805387998e33c9739b184b2d
David Molineus <david.molineus@netzmacht.de>
2015-11-12 09:22:31 +0100

    Introduce model specification.

---
+++ src/Data/Model/Specification.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+namespace Netzmacht\Contao\Toolkit\Data\Model;
+
+use Contao\Model;
+
+/**
+ * Specification defines search criteria and translate them into queries.
+ *
+ * @package Netzmacht\Contao\Toolkit\Data\Model
+ */
+interface Specification
+{
+    /**
+     * Consider if the specification is satisfied by the given model.
+     *
+     * @param Model $model Given model.
+     *
+     * @return bool
+     */
+    public function isSatisfiedBy(Model $model);
+
+    /**
+     * Transform the specification into an model query.
+     *
+     * @param array $columns Columns array.
+     * @param array $values  Values array.
+     *
+     * @return void
+     */
+    public function buildQuery(array &$columns, array &$values);
+}
