5b905e3c105dc860ee2c143beb1e344ef2ad9ce3
David Molineus <david.molineus@netzmacht.de>
2017-07-07 09:07:42 +0200

    Start creating a bundle.

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @package    netzmacht
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2017 netzmacht David Molineus. All rights reserved.
+ * @filesource
+ *
+ */
+
+namespace Netzmacht\Contao\Toolkit\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\CoreBundle\HttpKernel\Bundle\ContaoModuleBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Netzmacht\Contao\Toolkit\NetzmachtContaoToolkitBundle;
+
+/**
+ * Class Plugin.
+ *
+ * @package Netzmacht\Contao\Toolkit\ContaoManager
+ */
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(NetzmachtContaoToolkitBundle::class)
+                ->setReplace(['toolkit'])
+                ->setLoadAfter([ContaoCoreBundle::class, ContaoModuleBundle::class])
+        ];
+    }
+}
---
+++ src/NetzmachtContaoToolkitBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package    netzmacht
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2017 netzmacht David Molineus. All rights reserved.
+ * @filesource
+ *
+ */
+
+namespace Netzmacht\Contao\Toolkit;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class NetzmachtContaoToolkitBundle.
+ *
+ * @package Netzmacht\Contao\Toolkit
+ */
+class NetzmachtContaoToolkitBundle extends Bundle
+{
+
+}
