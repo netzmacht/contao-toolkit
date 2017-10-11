dd8b954ed9b1ed248edc38216042b72c1287dfee
David Molineus <david.molineus@netzmacht.de>
2017-10-11 15:59:23 +0200

    Add more specs.

---
+++ spec/Netzmacht/Contao/Toolkit/Bundle/DependencyInjection/Compiler/TranslatorPassSpec.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace spec\Netzmacht\Contao\Toolkit\Bundle\DependencyInjection\Compiler;
+
+use Netzmacht\Contao\Toolkit\Bundle\DependencyInjection\Compiler\TranslatorPass;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+class TranslatorPassSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(TranslatorPass::class);
+    }
+
+    function it_is_a_compiler_pass()
+    {
+        $this->shouldImplement(CompilerPassInterface::class);
+    }
+
+    function it_creates_translator_definition_if_contao_translator_does_not_exists(ContainerBuilder $container)
+    {
+        $container->hasDefinition('contao.translation.translator')->willReturn(false);
+        $container->setDefinition(
+            'netzmacht.contao_toolkit.translation.translator',
+            Argument::type(Definition::class)
+        )->shouldBeCalled();
+
+        $this->process($container);
+    }
+
+    function it_does_not_process_if_contao_translator_exists(ContainerBuilder $container)
+    {
+        $container->hasDefinition('contao.translation.translator')->willReturn(true);
+        $container->setDefinition(
+            'netzmacht.contao_toolkit.translation.translator',
+            Argument::type(Definition::class)
+        )->shouldNotBeCalled();
+
+        $this->process($container);
+    }
+}
