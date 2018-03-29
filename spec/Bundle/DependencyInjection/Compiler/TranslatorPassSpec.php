23ebedf60c843b60e4d4157022d9b123e7803b7f
David Molineus <david.molineus@netzmacht.de>
2018-03-29 10:06:17 +0200

    Make the builds pass.

--- spec/Bundle/DependencyInjection/Compiler/TranslatorPassSpec.php
+++ spec/Bundle/DependencyInjection/Compiler/TranslatorPassSpec.php
@@ -8,6 +8,7 @@ use Prophecy\Argument;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 
 class TranslatorPassSpec extends ObjectBehavior
 {
@@ -21,8 +22,14 @@ class TranslatorPassSpec extends ObjectBehavior
         $this->shouldImplement(CompilerPassInterface::class);
     }
 
-    function it_creates_translator_definition_if_contao_translator_does_not_exists(ContainerBuilder $container)
+    function it_creates_translator_definition_if_contao_translator_does_not_exists(ContainerBuilder $container, Definition $definition, ParameterBag $bag)
     {
+        $container->has('translator')->willReturn(true);
+
+        $container->findDefinition('translator')->willReturn($definition);
+        $container->getParameterBag()->willReturn($bag);
+
+
         $container->hasDefinition('contao.translation.translator')->willReturn(false);
         $container->setDefinition(
             'netzmacht.contao_toolkit.translation.translator',
@@ -32,8 +39,13 @@ class TranslatorPassSpec extends ObjectBehavior
         $this->process($container);
     }
 
-    function it_does_not_process_if_contao_translator_exists(ContainerBuilder $container)
+    function it_does_not_process_if_contao_translator_exists(ContainerBuilder $container, Definition $definition, ParameterBag $bag)
     {
+        $container->has('translator')->willReturn(true);
+
+        $container->findDefinition('translator')->willReturn($definition);
+        $container->getParameterBag()->willReturn($bag);
+
         $container->hasDefinition('contao.translation.translator')->willReturn(true);
         $container->setDefinition(
             'netzmacht.contao_toolkit.translation.translator',
