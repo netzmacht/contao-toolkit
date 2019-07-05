e6f43ba1353ae8fe1e414fbbf632eb200789ece0
David Molineus <david.molineus@netzmacht.de>
2019-07-05 10:10:21 +0200

    Use the request scope matcher to determine backend and frontend request
    Instead of relying on the constants the request scope matcher is passed
    as constructor arguments. Due to backwards compatibility the TL_MODE
    constant is used as fallback.

--- spec/Component/ContentElement/AbstractContentElementSpec.php
+++ spec/Component/ContentElement/AbstractContentElementSpec.php
@@ -13,9 +13,13 @@
 namespace spec\Netzmacht\Contao\Toolkit\Component\ContentElement;
 
 use Netzmacht\Contao\Toolkit\Component\ContentElement\AbstractContentElement;
+use Netzmacht\Contao\Toolkit\Routing\RequestScopeMatcher;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Symfony\Component\Templating\EngineInterface;
+use Netzmacht\Contao\Toolkit\Component\Component;
+use Netzmacht\Contao\Toolkit\Component\ContentElement\ContentElement;
+use function time;
 
 if (!defined('BE_USER_LOGGED_IN')) {
     define('BE_USER_LOGGED_IN', false);
@@ -33,7 +37,7 @@ class AbstractContentElementSpec extends ObjectBehavior
 
     private $modelData;
 
-    function let(EngineInterface $templateEngine)
+    function let(EngineInterface $templateEngine, RequestScopeMatcher $requestScopeMatcher)
     {
         $this->modelData = [
             'type' => 'test',
@@ -45,27 +49,104 @@ class AbstractContentElementSpec extends ObjectBehavior
 
         $this->model = new Model($this->modelData);
 
-        $this->beAnInstanceOf('spec\Netzmacht\Contao\Toolkit\Component\ContentElement\ConcreteContentElement');
-        $this->beConstructedWith($this->model, $templateEngine);
+        $requestScopeMatcher->isFrontendRequest()->willReturn(true);
+
+        $this->beAnInstanceOf(ConcreteContentElement::class);
+        $this->beConstructedWith($this->model, $templateEngine, 'main', $requestScopeMatcher);
     }
 
     function it_is_initializable()
     {
-        $this->shouldHaveType('Netzmacht\Contao\Toolkit\Component\ContentElement\AbstractContentElement');
+        $this->shouldHaveType(AbstractContentElement::class);
     }
 
     function it_is_a_component()
     {
-        $this->shouldImplement('Netzmacht\Contao\Toolkit\Component\Component');
+        $this->shouldImplement(Component::class);
     }
 
     function it_is_a_content_element()
     {
-        $this->shouldImplement('Netzmacht\Contao\Toolkit\Component\ContentElement\ContentElement');
+        $this->shouldImplement(ContentElement::class);
     }
 
     function it_generates_output(EngineInterface $templateEngine)
     {
+        $templateEngine->render(Argument::cetera())->willReturn('output');
+
+        $this->generate()->shouldBeString();
+        $this->generate()->shouldReturn('output');
+    }
+
+    function it_doesnt_generate_output_if_invisible(
+        EngineInterface $templateEngine,
+        RequestScopeMatcher $requestScopeMatcher
+    ) {
+        $this->beConstructedWith($this->model, $templateEngine, 'main', $requestScopeMatcher);
+
+        $this->model->invisible = true;
+
+        $templateEngine->render(Argument::cetera())->shouldNotBeCalled();
+
+        $this->generate()->shouldBeString();
+        $this->generate()->shouldReturn('');
+    }
+
+    function it_doesnt_generate_output_if_starts_in_future(
+        EngineInterface $templateEngine,
+        RequestScopeMatcher $requestScopeMatcher
+    ) {
+        $this->beConstructedWith($this->model, $templateEngine, 'main', $requestScopeMatcher);
+
+        $this->model->start = time() + 3600;
+
+        $templateEngine->render(Argument::cetera())->shouldNotBeCalled();
+
+        $this->generate()->shouldBeString();
+        $this->generate()->shouldReturn('');
+    }
+
+    function it_doesnt_generate_output_if_stopped_in_past(
+        EngineInterface $templateEngine,
+        RequestScopeMatcher $requestScopeMatcher
+    ) {
+        $this->beConstructedWith($this->model, $templateEngine, 'main', $requestScopeMatcher);
+
+        $this->model->start = time() - 3600;
+        $this->model->stop = time() - 3600;
+
+        $templateEngine->render(Argument::cetera())->shouldNotBeCalled();
+
+        $this->generate()->shouldBeString();
+        $this->generate()->shouldReturn('');
+    }
+
+    function it_ignores_visibility_settings_on_non_frontend_request(
+        EngineInterface $templateEngine,
+        RequestScopeMatcher $requestScopeMatcher
+    ) {
+        $this->beConstructedWith($this->model, $templateEngine, 'main', $requestScopeMatcher);
+
+        $requestScopeMatcher->isFrontendRequest()->willReturn(false);
+
+        $this->model->invisible = true;
+
+        $templateEngine->render(Argument::cetera())->willReturn('output');
+
+        $this->generate()->shouldBeString();
+        $this->generate()->shouldReturn('output');
+    }
+
+    function it_ignores_visibility_settings_on_preview_mode(
+        EngineInterface $templateEngine,
+        RequestScopeMatcher $requestScopeMatcher
+    ) {
+        $this->beConstructedWith($this->model, $templateEngine, 'main', $requestScopeMatcher, true);
+
+        $requestScopeMatcher->isFrontendRequest()->willReturn(true);
+
+        $this->model->invisible = true;
+
         $templateEngine->render(Argument::cetera())->willReturn('output');
 
         $this->generate()->shouldBeString();
--- src/Component/ContentElement/AbstractContentElement.php
+++ src/Component/ContentElement/AbstractContentElement.php
@@ -15,7 +15,16 @@ declare(strict_types=1);
 
 namespace Netzmacht\Contao\Toolkit\Component\ContentElement;
 
+use Contao\Database\Result;
+use Contao\Model;
+use Contao\Model\Collection;
+use InvalidArgumentException;
 use Netzmacht\Contao\Toolkit\Component\AbstractComponent;
+use Netzmacht\Contao\Toolkit\Routing\RequestScopeMatcher;
+use Symfony\Component\Templating\EngineInterface as TemplateEngine;
+use function trigger_error;
+use const BE_USER_LOGGED_IN;
+use const E_USER_DEPRECATED;
 
 /**
  * Class AbstractContentElement.
@@ -24,6 +33,47 @@ use Netzmacht\Contao\Toolkit\Component\AbstractComponent;
  */
 abstract class AbstractContentElement extends AbstractComponent implements ContentElement
 {
+    /**
+     * State if the request is called in the preview mode.
+     *
+     * @var bool|null
+     */
+    private $isPreviewMode;
+
+    /**
+     * AbstractContentElement constructor.
+     *
+     * @param Model|Collection|Result  $model               Object model or result.
+     * @param TemplateEngine           $templateEngine      Template engine.
+     * @param string                   $column              Column.
+     * @param RequestScopeMatcher|null $requestScopeMatcher Request scope matcher.
+     * @param bool|null                $isPreviewMode       State if the request is called in the preview mode.
+     *
+     * @throws InvalidArgumentException When model does not have a row method.
+     */
+    public function __construct(
+        $model,
+        TemplateEngine $templateEngine,
+        $column = 'main',
+        ?RequestScopeMatcher $requestScopeMatcher = null,
+        ?bool $isPreviewMode = null
+    ) {
+        parent::__construct($model, $templateEngine, $column, $requestScopeMatcher);
+
+        if ($isPreviewMode === null) {
+            // @codingStandardsIgnoreStart
+            @trigger_error(
+                'isPreviewMode not passed as fifth argument. isPreviewMode will be required in version 4.0.0',
+                E_USER_DEPRECATED
+            );
+            // @codingStandardsIgnoreEnd
+
+            $isPreviewMode = BE_USER_LOGGED_IN;
+        }
+
+        $this->isPreviewMode = $isPreviewMode;
+    }
+
     /**
      * {@inheritDoc}
      */
@@ -43,7 +93,7 @@ abstract class AbstractContentElement extends AbstractComponent implements Conte
      */
     protected function isVisible(): bool
     {
-        if (TL_MODE !== 'FE' || BE_USER_LOGGED_IN) {
+        if ($this->isPreviewMode || !$this->isFrontendRequest()) {
             return true;
         }
 
