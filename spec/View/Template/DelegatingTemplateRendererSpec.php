e8d9a62f4b2f2e1e39b8b0f6b37d36dea23b4910
David Molineus <david.molineus@netzmacht.de>
2026-09-15 10:27:25 +0200

    Add deprecation-spec helper, deprecate legacy Contao template rendering path

--- spec/View/Template/DelegatingTemplateRendererSpec.php
+++ spec/View/Template/DelegatingTemplateRendererSpec.php
@@ -11,9 +11,12 @@ use Netzmacht\Contao\Toolkit\View\Template\DelegatingTemplateRenderer;
 use Netzmacht\Contao\Toolkit\View\Template\TemplateFactory;
 use Netzmacht\Contao\Toolkit\View\Template\TemplateRenderer;
 use PhpSpec\ObjectBehavior;
+use spec\Netzmacht\Contao\Toolkit\DeprecationSpecHelper;
 
 final class DelegatingTemplateRendererSpec extends ObjectBehavior
 {
+    use DeprecationSpecHelper;
+
     public function let(TemplateFactory $templateFactory): void
     {
         $this->beConstructedWith($templateFactory);
@@ -75,5 +78,39 @@ final class DelegatingTemplateRendererSpec extends ObjectBehavior
     public function it_throws_if_twig_is_not_available(): void
     {
         $this->shouldThrow(RuntimeException::class)->during('render', ['foo.html.twig']);
+    }
+
+    public function it_triggers_a_deprecation_warning_when_rendering_a_contao_template(
+        TemplateFactory $templateFactory,
+        Template $template,
+    ): void {
+        $templateFactory->createBackendTemplate('foo', [])->willReturn($template);
+        $template->parse()->willReturn('foo_html');
+
+        $messages = $this->captureDeprecations(function (): void {
+            $this->render('be:foo');
+        });
+
+        $this->assertDeprecationTriggered($messages, 'Rendering legacy Contao templates via "be:foo"');
+    }
+
+    public function it_does_not_trigger_a_deprecation_warning_when_rendering_a_twig_template(
+        TemplateFactory $templateFactory,
+    ): void {
+        $templateFactory->createFrontendTemplate()->shouldNotBeCalled();
+
+        // A missing twig environment still throws RuntimeException, but no deprecation is
+        // triggered for the twig branch itself.
+        $messages = $this->captureDeprecations(function (): void {
+            try {
+                $this->render('foo.html.twig');
+            } catch (RuntimeException) {
+                // Expected: no twig environment configured in this spec's let().
+            }
+        });
+
+        if ($messages !== []) {
+            throw new \RuntimeException('Did not expect a deprecation warning for a twig template.');
+        }
     }
 }
